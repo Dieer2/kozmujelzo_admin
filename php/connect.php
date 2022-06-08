29e168438fa9ec2d671691116dde4eb6bff62f23
Gergely Kemény <gergely.kemeny@gmail.com>
2022-06-08 22:16:43 +0200

    commit

---
+++ php/connect.php
@@ -0,0 +1,14 @@
+<?php
+    $serverData = parse_ini_file("../php.ini");
+
+    $host = $serverData['host'];
+    $username = $serverData['user'];
+    $password = $serverData['pass'];
+    $dbname = $serverData['name'];
+
+    $connection = new mysqli($host, $username, $password, $dbname);
+
+    if($connection->connect_error) {
+        die("Nem sikerült az adatbázishoz való kapcsolódás: ". $connection->connect_error);
+    }
+?>

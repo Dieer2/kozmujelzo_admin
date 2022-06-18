cc0d5e0393251ec17c191d3f509381d5d44ffb36
Gergely Kemény <gergely.kemeny@gmail.com>
2022-06-18 13:57:46 +0200

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,18 @@
 
 $(document).ready(function () {
 
-    function getTermekek() {
+    function preventFormResubmit() {
+        if (window.history.replaceState) {
+            window.history.replaceState(null, null, window.location.href);
+        }
+    }
+
+    function createTermekekModule() {
+        showTermekek();
+        newTermek();
+    }
+
+    function showTermekek() {
         $.ajax({
             type: `GET`,
             url: `php/termekekquery.php`,
@@ -13,40 +24,51 @@ $(document).ready(function () {
         });
     }
 
-    function getTermekWindow(caller) {
-        $(caller).after(
-            `<form action="" method="post">
-                <p id="termekWindowId">Id: </p>
-                <label for="termekWindowNev">Termék név: </label>
-                <input id="termekWindowNev" type="text" />
-                <label for="termekWindowSzin">Termék szín: </label>
-                <select id="termekWindowSzin" type="text">
-                </select>
-                <label for="termekWindowMinta">Termék minta: </label>
-                <select id="termekWindowMinta" type="text">
-                </select>
-                <label for="termekWindowRaktaron">Raktáron: </label>
-                <input id="termekWindowRaktaron" type="number" />
-                <label for="termekWindowLeiras">Termék leírás: </label>
-                <textarea id="termekWindowLeiras"></textarea>
-                <label for="termekWindowKep">Termék képe: </label>
-                <input accept="image/*" id="termekWindowKep" type="file" />
-                <label for="termekWindowKepek">További képek: </label>
-                <input accept="image/*" id="termekWindowKepek" type="file" multiple />
-                <input type="submit" value="Mentés" />
-            </form>`
-        );
+    function newTermek() {
+        $(`#newTermekButton`).click(function () {
+            showTermekWindow(`#newTermekButton`);
+            $(`#newTermekButton`).attr(`disabled`, true);
+            cancelTermekWindow();
+        });
     }
 
-    function newTermek() {
+    function cancelTermekWindow() {
+        $(`#cancelTermekWindowButton`).click(function () {
+            $(`#termekWindow`).remove();
+            $(`#newTermekButton`).attr(`disabled`, false);
+        });
+    }
 
+    function showTermekWindow(caller) {
+        $(`${caller}`).after(`
+        <form id="termekWindow" action="" method="post">
+            <p id="termekWindowId">Id: </p>
+            <label for="termekWindowNev">Termék név: </label>
+            <input id="termekWindowNev" type="text" />
+            <label for="termekWindowSzin">Termék szín: </label>
+            <select id="termekWindowSzin" type="text">
+            </select>
+            <label for="termekWindowMinta">Termék minta: </label>
+            <select id="termekWindowMinta" type="text">
+            </select>
+            <label for="termekWindowRaktaron">Raktáron: </label>
+            <input id="termekWindowRaktaron" type="number" />
+            <label for="termekWindowLeiras">Termék leírás: </label>
+            <textarea id="termekWindowLeiras"></textarea>
+            <label for="termekWindowKep">Termék képe: </label>
+            <input accept="image/*" id="termekWindowKep" type="file" />
+            <label for="termekWindowKepek">További képek: </label>
+            <input accept="image/*" id="termekWindowKepek" type="file" multiple />
+            <button id="cancelTermekWindowButton" type="button">Mégse</button>
+            <input type="submit" value="Hozzáad" />
+        </form>`
+        );
     }
 
     function setTermek() {
 
     }
 
-    getTermekek();
-    getTermekWindow("#newTermekButton")
-
+    preventFormResubmit();
+    createTermekekModule();
 });
